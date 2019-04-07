73c5fa161019fca4829f3af66bcf153bfc5a7287
csh <tizkzks96@gmail.com>
2019-04-07 21:03:55 +0900

    bullets bug fix

--- SenierProject/Assets/Script/BulletCtrl.cs
+++ SenierProject/Assets/Script/BulletCtrl.cs
@@ -6,7 +6,7 @@ public class BulletCtrl : MonoBehaviour {
     int activeCount = 0;
     public List<GameObject> bullets = new List<GameObject>();
     float time;
-    float continuousTime = 10.0f;
+    public float continuousTime = 1.5f;
 
     private void OnEnable() {
         time = 0;
@@ -14,23 +14,27 @@ public class BulletCtrl : MonoBehaviour {
     }
 
     private void Update() {
-        time += Time.deltaTime;
-        if (time > continuousTime) {
-            gameObject.SetActive(false);
-        }
+        //time += Time.deltaTime;
+        //if (time > continuousTime) {
+        //    gameObject.SetActive(false);
+        //}
         
     }
-    //public void ActiveCount() {
-    //    //print("activeCount : " + activeCount);
-    //    if (activeCount < transform.childCount-1)
-    //        activeCount++;
-    //    else {
-    //        print(activeCount);
-    //        activeCount = 0;
+    public void ActiveCount() {
+        //print("activeCount : " + activeCount);
+        if (activeCount < transform.childCount - 1)
+            activeCount++;
+        else {
+            print(activeCount);
+            activeCount = 0;
+            Invoke("ActiveFalse", 1.6f);
             
-    //    }
-    //}
+        }
+    }
 
+    public void ActiveFalse() {
+        gameObject.SetActive(false);
+    }
     public void SetBulletList() {
         
         int iCount = transform.childCount;
