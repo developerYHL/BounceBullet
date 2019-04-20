a729dfd3b967cd209064ea7d3317197b580da258
csh <tizkzks96@gmail.com>
2019-04-20 21:42:23 +0900

    스크립트 네임스페이스 ClientLibrary 추가

--- SenierProject/Assets/Script/BulletCtrl.cs
+++ SenierProject/Assets/Script/BulletCtrl.cs
@@ -2,99 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletCtrl : MonoBehaviour {
+namespace ClientLibrary
+{
+    public class BulletCtrl : MonoBehaviour
+    {
 
-    public int maxReflectionCount = 5;
-    public float maxStepDistance = 200;
-    public LayerMask blockingLayer;
+        public int maxReflectionCount = 5;
+        public float maxStepDistance = 200;
+        public LayerMask blockingLayer;
 
-    int reflectCount = 0;
-    // Adjust the speed for the application.
-    public float speed;
+        int reflectCount = 0;
+        // Adjust the speed for the application.
+        public float speed;
 
-    // The target (cylinder) position.
-    private Transform target;
-    //float continuousTime;
+        // The target (cylinder) position.
+        private Transform target;
+        //float continuousTime;
 
-    TrailRenderer trailRenderer;
+        TrailRenderer trailRenderer;
 
-    float time;
+        float time;
 
-    List<Vector3> reflectPositions = new List<Vector3>();
+        List<Vector3> reflectPositions = new List<Vector3>();
 
-    private void Start() {
-        CalculateTarget();
-        //GetComponent<TrailRenderer>().time = continuousTime;
-    }
-
-    private void Update() {
-        if (reflectCount < maxReflectionCount) {
-            MoveObject();
-        }
-        else if (reflectCount == maxReflectionCount) {
-            reflectCount++;
-        };
-    }
-
-    private void DrawPredictedReflectionPattern(Vector3 position, Vector3 direction, int reflectionsRemaining) {
-        if (reflectionsRemaining == 0) {
-            return;
+        private void Start() {
+            CalculateTarget();
+            //GetComponent<TrailRenderer>().time = continuousTime;
         }
 
-        Ray ray = new Ray(position, direction);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, maxStepDistance, blockingLayer)) {
-            direction = Vector3.Reflect(direction, hit.normal);
-            position = hit.point;
-
-        }
-        else {
-            position += direction * maxStepDistance;
-            //이동이 끝나면 사라져야함
-        }
-
-        reflectPositions.Add(position);
-        DrawPredictedReflectionPattern(position, direction, reflectionsRemaining - 1);
-    }
-
-    private void OnDrawGizmosSelected() {
-        // Draws a 5 unit long red line in front of the object
-        Gizmos.color = Color.red;
-        Vector3 direction = transform.forward * 25;
-        Gizmos.DrawRay(transform.position, direction);
-    }
-
-    public void MoveObject() {
-        //ReflectionCount를 다 소모했을 때 실행
-        if (reflectCount >= maxReflectionCount) {
-        }
-        else {
-            // Move our position a step closer to the target.
-            float step = speed * Time.deltaTime; // calculate distance to move
-            
-            transform.position = Vector3.MoveTowards(transform.position, reflectPositions[reflectCount], step);
-
-            // Check if the position of the cube and sphere are approximately equal.
-            if (Vector3.Distance(transform.position, reflectPositions[reflectCount]) < 0.001f) {
-                // Swap the position of the cylinder.
+        private void Update() {
+            if (reflectCount < maxReflectionCount) {
+                MoveObject();
+            }
+            else if (reflectCount == maxReflectionCount) {
                 reflectCount++;
-                //DrawPredictedReflectionPattern(this.transform.position + this.transform.forward * 0.75f, this.transform.forward, maxReflectionCount);
+            };
+        }
+
+        private void DrawPredictedReflectionPattern(Vector3 position, Vector3 direction, int reflectionsRemaining) {
+            if (reflectionsRemaining == 0) {
+                return;
+            }
+
+            Ray ray = new Ray(position, direction);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, maxStepDistance, blockingLayer)) {
+                direction = Vector3.Reflect(direction, hit.normal);
+                position = hit.point;
+
+            }
+            else {
+                position += direction * maxStepDistance;
+                //이동이 끝나면 사라져야함
+            }
+
+            reflectPositions.Add(position);
+            DrawPredictedReflectionPattern(position, direction, reflectionsRemaining - 1);
+        }
+
+        private void OnDrawGizmosSelected() {
+            // Draws a 5 unit long red line in front of the object
+            Gizmos.color = Color.red;
+            Vector3 direction = transform.forward * 25;
+            Gizmos.DrawRay(transform.position, direction);
+        }
+
+        public void MoveObject() {
+            //ReflectionCount를 다 소모했을 때 실행
+            if (reflectCount >= maxReflectionCount) {
+            }
+            else {
+                // Move our position a step closer to the target.
+                float step = speed * Time.deltaTime; // calculate distance to move
+
+                transform.position = Vector3.MoveTowards(transform.position, reflectPositions[reflectCount], step);
+
+                // Check if the position of the cube and sphere are approximately equal.
+                if (Vector3.Distance(transform.position, reflectPositions[reflectCount]) < 0.001f) {
+                    // Swap the position of the cylinder.
+                    reflectCount++;
+                    //DrawPredictedReflectionPattern(this.transform.position + this.transform.forward * 0.75f, this.transform.forward, maxReflectionCount);
+                }
             }
         }
-    }
 
-    public void CalculateTarget() {
-        //ClearBullet();
-        if (reflectPositions.Count == 0)
-            DrawPredictedReflectionPattern(transform.position, transform.forward, maxReflectionCount);
-        else {
-            print("not Null");
+        public void CalculateTarget() {
+            //ClearBullet();
+            if (reflectPositions.Count == 0)
+                DrawPredictedReflectionPattern(transform.position, transform.forward, maxReflectionCount);
+            else {
+                print("not Null");
+            }
         }
-    }
 
-    public void ClearBullet() {
-        reflectPositions.Clear();
-        transform.position = transform.parent.position;
-        reflectCount = 0;
+        public void ClearBullet() {
+            reflectPositions.Clear();
+            transform.position = transform.parent.position;
+            reflectCount = 0;
+        }
     }
 }
--- SenierProject/Assets/Script/PlayerController.cs
+++ SenierProject/Assets/Script/PlayerController.cs
@@ -1,109 +1,113 @@
 ﻿using UnityEngine;
 using System.Collections;
+namespace ClientLibrary
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float moveSpeed;
+        private Rigidbody myRigidbody;
 
-[RequireComponent (typeof (Animator))]
-public class PlayerController : MonoBehaviour {
-    public float moveSpeed;
-    private Rigidbody myRigidbody;
+        private Vector3 moveInput;
+        private Vector3 moveVelocity;
 
-    private Vector3 moveInput;
-    private Vector3 moveVelocity;
+        private Camera mainCamra;
 
-    private Camera mainCamra;
+        public Transform rightGunBone;
+        public Transform leftGunBone;
+        public Arsenal[] arsenal;
 
-    public Transform rightGunBone;
-	public Transform leftGunBone;
-	public Arsenal[] arsenal;
+        private Animator animator;
 
-	private Animator animator;
+        public GunCtrl theGun;
 
-    public GunCtrl theGun;
+        void Awake() {
 
-    void Awake() {
-        
-        myRigidbody = GetComponent<Rigidbody>();
-        mainCamra = FindObjectOfType<Camera>();
-		animator = GetComponent<Animator> ();
-		if (arsenal.Length > 0)
-			SetArsenal (arsenal[0].name);
-	}
-    private void Start() {
-        animator.SetBool("Aiming", false);
-        SetArsenal("Rifle");
-    }
+            myRigidbody = GetComponent<Rigidbody>();
+            mainCamra = FindObjectOfType<Camera>();
+            animator = GetComponent<Animator>();
+            if (arsenal.Length > 0)
+                SetArsenal(arsenal[0].name);
+        }
+        private void Start() {
+            animator.SetBool("Aiming", false);
+            SetArsenal("Rifle");
+        }
 
-    private void Update() {
+        private void Update() {
 #if UNITY_STANDALONE || UNITY_WEBPLAYER
 
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
-        if(moveInput != null) {
-            animator.speed = 0.5f;
-        }
-        moveVelocity = moveInput * moveSpeed;
+            if (moveInput != null) {
+                animator.speed = 0.5f;
+            }
+            moveVelocity = moveInput * moveSpeed;
 
-        Ray cameraRay = mainCamra.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayLength;
+            Ray cameraRay = mainCamra.ScreenPointToRay(Input.mousePosition);
+            Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+            float rayLength;
 
-        if (groundPlane.Raycast(cameraRay, out rayLength)) {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+            if (groundPlane.Raycast(cameraRay, out rayLength)) {
+                Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+                Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
 
-            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-        }
+                transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+            }
 
-        if (Input.GetMouseButtonDown(0)) {
-            animator.SetBool("Aiming", true);
-            animator.SetTrigger("Attack");
-            theGun.isFireing = true;
-        }
-        if (Input.GetMouseButtonUp(0)) {
-            animator.SetBool("Aiming", false);
-            theGun.isFireing = false;
-        }
+            if (Input.GetMouseButtonDown(0)) {
+                animator.SetBool("Aiming", true);
+                animator.SetTrigger("Attack");
+                theGun.isFireing = true;
+            }
+            if (Input.GetMouseButtonUp(0)) {
+                animator.SetBool("Aiming", false);
+                theGun.isFireing = false;
+            }
 #endif
-    }
+        }
 
-    private void FixedUpdate() {
-        myRigidbody.velocity = moveVelocity;
-    }
+        private void FixedUpdate() {
+            myRigidbody.velocity = moveVelocity;
+        }
 
-    public void SetArsenal(string name) {
-		foreach (Arsenal hand in arsenal) {
-			if (hand.name == name) {
-				if (rightGunBone.childCount > 0)
-					Destroy(rightGunBone.GetChild(0).gameObject);
-				if (leftGunBone.childCount > 0)
-					Destroy(leftGunBone.GetChild(0).gameObject);
-				if (hand.rightGun != null) {
-					GameObject newRightGun = (GameObject) Instantiate(hand.rightGun);
-					newRightGun.transform.parent = rightGunBone;
-					newRightGun.transform.localPosition = Vector3.zero;
-					newRightGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
-                    theGun = newRightGun.GetComponent<GunCtrl>();
-                    theGun.firePoint= newRightGun.transform;
-                    print(newRightGun);
+        public void SetArsenal(string name) {
+            foreach (Arsenal hand in arsenal) {
+                if (hand.name == name) {
+                    if (rightGunBone.childCount > 0)
+                        Destroy(rightGunBone.GetChild(0).gameObject);
+                    if (leftGunBone.childCount > 0)
+                        Destroy(leftGunBone.GetChild(0).gameObject);
+                    if (hand.rightGun != null) {
+                        GameObject newRightGun = (GameObject)Instantiate(hand.rightGun);
+                        newRightGun.transform.parent = rightGunBone;
+                        newRightGun.transform.localPosition = Vector3.zero;
+                        newRightGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+                        theGun = newRightGun.GetComponent<GunCtrl>();
+                        theGun.firePoint = newRightGun.transform;
+                        print(newRightGun);
                     }
-				if (hand.leftGun != null) {
-					GameObject newLeftGun = (GameObject) Instantiate(hand.leftGun);
-					newLeftGun.transform.parent = leftGunBone;
-					newLeftGun.transform.localPosition = Vector3.zero;
-					newLeftGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
-                    theGun = newLeftGun.GetComponent<GunCtrl>();
-                }
-				animator.runtimeAnimatorController = hand.controller;
-                
-				return;
-				}
-		}
-	}
+                    if (hand.leftGun != null) {
+                        GameObject newLeftGun = (GameObject)Instantiate(hand.leftGun);
+                        newLeftGun.transform.parent = leftGunBone;
+                        newLeftGun.transform.localPosition = Vector3.zero;
+                        newLeftGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+                        theGun = newLeftGun.GetComponent<GunCtrl>();
+                    }
+                    animator.runtimeAnimatorController = hand.controller;
 
-	[System.Serializable]
-	public struct Arsenal {
-		public string name;
-		public GameObject rightGun;
-		public GameObject leftGun;
-		public RuntimeAnimatorController controller;
-	}
+                    return;
+                }
+            }
+        }
+
+        [System.Serializable]
+        public struct Arsenal
+        {
+            public string name;
+            public GameObject rightGun;
+            public GameObject leftGun;
+            public RuntimeAnimatorController controller;
+        }
+    }
 }
