22cd0176e37b7523195bb62342413dbb67a2ae9c
csh <tizkzks96@gmail.com>
2019-04-23 00:19:45 +0900

    joystic 연동 및 활성화 버튼 추가

---
+++ SenierProject/Assets/Script/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ClientLibrary
+{
+
+    public class UIManager : MonoBehaviour
+    {
+        bool useJoystic;
+        private void Start() {
+             useJoystic = GameObject.Find("/Sci-Fi_Soldier(Clone)").GetComponent<PlayerController>().useJoystic;
+        }
+        public void UseJoysticBtn() {
+            if(GameObject.Find("/Sci-Fi_Soldier(Clone)").GetComponent<PlayerController>().useJoystic == true) {
+                GameObject.Find("/Sci-Fi_Soldier(Clone)").GetComponent<PlayerController>().useJoystic = false;
+            }else
+                GameObject.Find("/Sci-Fi_Soldier(Clone)").GetComponent<PlayerController>().useJoystic = true;
+
+        }
+    }
+}
